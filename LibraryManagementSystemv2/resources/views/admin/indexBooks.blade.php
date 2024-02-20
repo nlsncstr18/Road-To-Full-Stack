73ab630c14f9cfd4414c5c508cf8de2d3c812beb
Nelson Castro <nlsncstr18@gmail.com>
2024-02-20 17:59:17 +0800

    Fixed index users

--- LibraryManagementSystemv2/resources/views/admin/indexBooks.blade.php
+++ LibraryManagementSystemv2/resources/views/admin/indexBooks.blade.php
@@ -8,11 +8,11 @@
             <thead class="text-md backdrop-blur-lg text-white uppercase text-center">
                 <tr>
                     <th scope="col" class="p-4">
-                        <div class="flex items-center">
+                        {{-- <div class="flex items-center">
                             <input id="checkbox-all-search" type="checkbox"
                                 class="w-4 h-4 text-white  border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                             <label for="checkbox-all-search" class="sr-only">checkbox</label>
-                        </div>
+                        </div> --}}
                     </th>
                     <th scope="col" class="px-6 py-3">
                         Title
@@ -44,11 +44,11 @@
                 @foreach ($books as $book)
                     <tr class="backdrop-blur-md hover:bg-gray-700 border-b  border-black text-center text-white ">
                         <td class="w-4 p-4">
-                            <div class="flex items-center">
+                            {{-- <div class="flex items-center">
                                 <input id="checkbox-table-search-2" type="checkbox"
                                     class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
-                            </div>
+                            </div> --}}
                         </td>
                         <td class="px-6 py-3">{{ $book->title }}</td>
                         <td class="px-6 py-3">{{ $book->author }}</td>
@@ -76,7 +76,6 @@
 
         </table>
     </div>
-
 
 
 
--- LibraryManagementSystemv2/resources/views/admin/indexUsers.blade.php
+++ LibraryManagementSystemv2/resources/views/admin/indexUsers.blade.php
@@ -6,11 +6,11 @@
             <thead class="text-md backdrop-blur-lg text-white uppercase text-center">
                 <tr>
                     <th scope="col" class="p-4 ">
-                        <div class="flex items-center">
+                        {{-- <div class="flex items-center">
                             <input id="checkbox-all-search" type="checkbox"
                                 class="w-4 h-4 text-white  border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                             <label for="checkbox-all-search" class="sr-only">checkbox</label>
-                        </div>
+                        </div> --}}
                     </th>
                     <th scope="col" class="px-6 py-3">
                         I.D
@@ -46,11 +46,11 @@
                     @if ($user->userType !== 1)
                         <tr class="backdrop-blur-md hover:bg-gray-700 border-b  border-black text-center text-white ">
                             <td class="w-4 p-4">
-                                <div class="flex items-center">
+                                {{-- <div class="flex items-center">
                                     <input id="checkbox-table-search-2" type="checkbox"
                                         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                     <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
-                                </div>
+                                </div> --}}
                             </td>
                             <td class="px-6 py-3">{{ $user->id }}</td>
                             <td class="px-6 py-3">{{ $user->name }}</td>
